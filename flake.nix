b37ea703814ba1f7c93913b796ac87c1eac6ba3e
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2025-11-08 17:47:54 +0100

    Fix flake.nix

--- flake.nix
+++ flake.nix
@@ -8,37 +8,56 @@
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      imports = [
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: let
 
-      perSystem = { pkgs, ... }: let
-        jdk = pkgs.jdk21;
-        buildInputs = [ jdk ];
-        devTools = [ pkgs.google-java-format ];
+        jdk = pkgs.jdk21.override {
+        };
+
+        buildInputs = [
+          jdk
+        ];
+
+        devTools = [
+          pkgs.google-java-format
+        ];
+
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = buildInputs ++ devTools;
         };
 
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "java-template";
-          version = "1.0.0";
-          src = ./.;
-          nativeBuildInputs = buildInputs ++ [ pkgs.makeWrapper ];
-          buildPhase = ''
-            javac -Werror -g:none -deprecation -verbose Main.java
-          '';
-          installPhase = ''
-            mkdir -p $out/{bin,lib}
-            cp *.class $out/lib/
-            makeWrapper ${pkgs.lib.getExe jdk} $out/bin/java-template \
-              --add-flags "-cp $out/lib/ Main"
-          '';
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "java-template";
+            version = "1.0.0";
+
+            src = ./.;
+
+            nativeBuildInputs = buildInputs ++ [ pkgs.makeWrapper ];
+                                    
+            buildPhase = ''
+              javac -Werror -g:none -deprecation -verbose Main.java
+            '';
+
+            installPhase = ''
+              mkdir -p $out/{bin,lib}
+              cp *.class $out/lib
+
+              makeWrapper ${pkgs.lib.getExe jdk} $out/bin/java-template --add-flags "-cp $out/lib/ Main"
+            '';
+          };
         };
       };
 
       templates.default = {
-        description = "Java project template";
+        description = "Java template";
         source = ./.;
+      };
+    
+      flake = {
       };
     };
 }
